93990e3424ca04536def99968af7cb3089090b4c
zizonyundi <43987280+zizonyundi@users.noreply.github.com>
2020-08-24 02:33:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
+
 # with open("templates/index.html") as fp:
 #     soup = BeautifulSoup(fp, 'html.parser')
 #     result = soup.find_all('p',class_="select")
@@ -18,24 +19,18 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+
 @app.route('/list', methods=['GET'])
 def show_sayings():
     sayings = list(db.shesaid.find({}, {'_id': False}))
     return jsonify({'result': 'success', 'sayings_list': sayings})
 
+
 @app.route('/category', methods=['GET'])
 def sort_sayings():
     sayings = request.args.get('category')
-    print(sayings)
     sayings = list(db.shesaid.find({"category": {"$in": [sayings]}}, {'_id': False}))
     return jsonify({'result': 'success', 'sayings_list': sayings})
-    # with open("templates/index.html") as fp:
-    #     soup = BeautifulSoup(fp, 'html.parser')
-        # result = soup.find_all('p', class_="select")
-        # for tag in result:
-        #     print(tag.text)
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
