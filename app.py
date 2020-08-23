f194c35a396995da9b534467f80f0b213900feec
zizonyundi <43987280+zizonyundi@users.noreply.github.com>
2020-08-23 13:02:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,9 @@ def sort_sayings():
         soup = BeautifulSoup(fp, 'html.parser')
         result = soup.find_all('p', class_="select")
         for tag in result:
-            sort_movie = list(db.shesaid.find({"category": {"$in": [tag.text]}}, {'_id': False}))
-            return jsonify({'result': 'success', 'sayings_list': sort_movie})
+            print(tag.text)
+            sayings = list(db.shesaid.find({"category": {"$in": [tag.text]}}, {'_id': False}))
+            return jsonify({'result': 'success', 'sayings_list': sayings})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
